06bda838bc39cf4fdc12e3af194426048974f94a
lucemia <lucemia@gmail.com>
2023-06-07 20:20:30 +0800

    filter out openai-organization

--- src/fn/conftest.py
+++ src/fn/conftest.py
@@ -3,8 +3,17 @@ from typing import Any
 import pytest
 
 
+def filter_response_headers(response: dict[str, Any]) -> dict[str, Any]:
+    # Modify or exclude headers as needed
+    response["headers"].pop("openai-organization", None)  # Exclude a header from the response
+    return response
+
+
 @pytest.fixture(scope="module")
 def vcr_config() -> dict[str, Any]:
     return {
-        "filter_headers": ["authorization"],  # Be sure to match the case of the header exactly
+        "filter_headers": [
+            "authorization",
+        ],  # Be sure to match the case of the header exactly
+        "before_record_response": filter_response_headers,
     }
