781c9cd8ebbca24640713751dbfc0db7e90d3a28
lucemia <lucemia@gmail.com>
2023-06-07 11:05:57 +0800

    add more test case

--- src/fn/tests/test_ai_function.py
+++ src/fn/tests/test_ai_function.py
@@ -1,4 +1,5 @@
 import pytest
+from pydantic import BaseModel
 from syrupy.assertion import SnapshotAssertion
 
 from ..ai_function import ai_fn
@@ -9,6 +10,25 @@ def fabnocci(n: int) -> int:  # type: ignore[empty-body]
     """return fabnocci number"""
 
 
+class Hero(BaseModel):
+    """Hero model."""
+
+    name: str
+    age: int
+
+
+@ai_fn
+def fake_hero(n: int) -> list[Hero]:  # type: ignore[empty-body]
+    """generate fake hero."""
+
+
 @pytest.mark.vcr(filter_headers=["authorization"])
-def test_ai_fn(snapshot: SnapshotAssertion) -> None:
+def test_ai_fabnocci(snapshot: SnapshotAssertion) -> None:
     assert snapshot == fabnocci(10)
+
+
+@pytest.mark.vcr(filter_headers=["authorization"])
+def test_ai_fake_hero() -> None:
+    heros = fake_hero(5)
+    assert len(heros) == 5
+    assert all(isinstance(k, Hero) for k in heros)
