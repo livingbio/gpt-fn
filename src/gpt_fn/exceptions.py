dc2d06475a5f7ae1b0881ba2f93c5890b1c2d8ea
vickyliin <vickyliinn@gmail.com>
2023-06-12 17:18:10 +0800

    black format

--- src/gpt_fn/exceptions.py
+++ src/gpt_fn/exceptions.py
@@ -8,9 +8,7 @@ class GptFnError(Exception):
 
 
 class CompletionIncompleteError(GptFnError):
-    def __init__(
-        self, msg: str, request: dict[str, Any], response: OpenAIObject
-    ) -> None:
+    def __init__(self, msg: str, request: dict[str, Any], response: OpenAIObject) -> None:
         super().__init__(msg)
         self.response = response
         self.request = request
