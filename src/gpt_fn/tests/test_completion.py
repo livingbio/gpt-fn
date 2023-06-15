41fb283f40d04575c5f8bad5911eb6ef2bd40104
lucemia <lucemia@gmail.com>
2023-06-15 16:46:34 +0800

    add exception handle if openai cannot find related function

--- src/gpt_fn/tests/test_completion.py
+++ src/gpt_fn/tests/test_completion.py
@@ -1,8 +1,40 @@
 import pytest
 from syrupy.assertion import SnapshotAssertion
 
-from ..completion import chat_completion
+from ..completion import chat_completion, function_completion
 from ..exceptions import CompletionIncompleteError
+
+
+def book_a_flight(date: str, destination: str, origin: str = "London") -> str:
+    return f"Booked a flight from {origin} to {destination} on {date}."
+
+
+@pytest.mark.vcr(match_on=["method", "scheme", "host", "port", "path", "query", "body"])
+def test_function_completion(snapshot: SnapshotAssertion) -> None:
+    msg = function_completion(
+        [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "I want to book a flight to London."},
+        ],
+        functions=[book_a_flight],
+    )
+    assert snapshot == msg
+
+
+@pytest.mark.vcr(match_on=["method", "scheme", "host", "port", "path", "query", "body"])
+def test_function_completion_without_neccess_function(
+    snapshot: SnapshotAssertion,
+) -> None:
+    with pytest.raises(CompletionIncompleteError) as excinfo:
+        msg = function_completion(
+            [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "I want to book a restaurant in London."},
+            ],
+            functions=[book_a_flight],
+        )
+    assert snapshot == excinfo.exconly()
+    assert snapshot == vars(excinfo.value)
 
 
 @pytest.mark.vcr(match_on=["method", "scheme", "host", "port", "path", "query", "body"])
