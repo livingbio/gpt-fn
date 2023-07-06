90bd323125e9f00533f08682eb305d743c524444
lucemia <lucemia@gmail.com>
2023-07-06 11:09:50 +0800

    add test case

--- src/gpt_fn/utils/tests/test_pydantic_parser.py
+++ src/gpt_fn/utils/tests/test_pydantic_parser.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 import pytest
 from pydantic import BaseModel, Field
 from syrupy.assertion import SnapshotAssertion
 
+from ...completion import Message, chat_completion
 from ..pydantic_parser import ParserError, PydanticParser
 
 
@@ -11,6 +14,22 @@ class Receipt(BaseModel):
     amount: float
     currency: str = Field(description="ISO 4217 currency code")
     customer: str
+
+
+class Email(BaseModel):
+    subject: str = Field(description="the subject of email")
+    body: str = Field(description="the body of email")
+
+
+@pytest.mark.vcr(match_on=["method", "scheme", "host", "port", "path", "query", "body"])
+@pytest.mark.parametrize("test_filename, model", [("email.txt", Email)])
+def test_pydantic_parser_with_prompt(snapshot: SnapshotAssertion, test_filename: str, model: type[BaseModel], datadir: Path) -> None:
+
+    content = (datadir / test_filename).read_text()
+    parser = PydanticParser[BaseModel](pydantic_model=model)
+
+    result = chat_completion(messages=[Message(role="user", content=content + f"\n{parser.get_format_instructions()}")])
+    assert snapshot == result
 
 
 def test_pydantic_parser_get_format_instructions(
