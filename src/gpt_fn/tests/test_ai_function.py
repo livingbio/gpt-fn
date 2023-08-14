40f6800edc75f1e5edae14a730a11d9718eaa077
vickyliin <vickyliinn@gmail.com>
2023-08-15 03:17:09 +0800

    add test

--- src/gpt_fn/tests/test_ai_function.py
+++ src/gpt_fn/tests/test_ai_function.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from syrupy.assertion import SnapshotAssertion
 
 from ..ai_function import ai_fn
+from ..exceptions import AiFnError
 
 
 @ai_fn
@@ -22,6 +23,11 @@ def fake_hero(n: int) -> list[Hero]:  # type: ignore[empty-body]
     """generate fake hero."""
 
 
+@ai_fn
+def generate_hashtags(text: str) -> list[str]:  # type: ignore[empty-body]
+    """generate social media hashtags from text."""
+
+
 @pytest.mark.vcr(match_on=["method", "scheme", "host", "port", "path", "query", "body"])
 def test_ai_fabnocci(snapshot: SnapshotAssertion) -> None:
     assert snapshot == fabnocci(10)
@@ -33,3 +39,13 @@ def test_ai_fake_hero(snapshot: SnapshotAssertion) -> None:
     assert len(heros) == 5
     assert all(isinstance(k, Hero) for k in heros)
     assert snapshot == heros
+
+
+@pytest.mark.vcr(match_on=["method", "scheme", "host", "port", "path", "query", "body"])
+def test_ai_fn_max_token(snapshot: SnapshotAssertion) -> None:
+    with pytest.raises(AiFnError) as excinfo:
+        generate_hashtags(" ".join("hello" for _ in range(5001)))
+
+    assert snapshot(name="exconly") == excinfo.exconly()
+    assert snapshot(name="context") == excinfo.value.__context__
+    assert snapshot(name="vars(exc)") == vars(excinfo.value)
