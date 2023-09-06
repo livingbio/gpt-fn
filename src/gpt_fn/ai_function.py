25f11fe53cb1f9643331aa2415c5a590755f653c
lucemia <lucemia@gmail.com>
2023-09-06 21:17:37 +0800

    fix #14, let ai_fn can config model

--- src/gpt_fn/ai_function.py
+++ src/gpt_fn/ai_function.py
@@ -12,27 +12,30 @@ T = TypeVar("T")
 P = ParamSpec("P")
 
 
-def ai_fn(
-    fn: Callable[P, T],
-) -> Callable[P, T]:
-    sig = FunctionSignature(fn)
+def ai_fn(model: str = "gpt-3.5-turbo") -> Callable[[Callable[P, T]], Callable[P, T]]:
+    def _ai_fn(
+        fn: Callable[P, T],
+    ) -> Callable[P, T]:
+        sig = FunctionSignature(fn)
 
-    @wraps(fn)
-    def inner(*args: Any, **kwargs: Any) -> T:
-        fn_call = sig.call_line(*args, **kwargs)
-        template = ChatTemplate(
-            messages=[
-                MessageTemplate(role="system", content=sig.instruction()),
-                MessageTemplate(role="user", content=fn_call),
-            ]
-        )
+        @wraps(fn)
+        def inner(*args: Any, **kwargs: Any) -> T:
+            fn_call = sig.call_line(*args, **kwargs)
+            template = ChatTemplate(
+                messages=[
+                    MessageTemplate(role="system", content=sig.instruction()),
+                    MessageTemplate(role="user", content=fn_call),
+                ]
+            )
 
-        try:
-            resp = chat_completion(template.render(), temperature=0.0)
-        except openai.error.InvalidRequestError as e:
-            fn_locals = sig.locals(*args, **kwargs)
-            raise AiFnError(fn_call, fn_locals) from e
+            try:
+                resp = chat_completion(template.render(), temperature=0.0, model=model)
+            except openai.error.InvalidRequestError as e:
+                fn_locals = sig.locals(*args, **kwargs)
+                raise AiFnError(fn_call, fn_locals) from e
 
-        return sig.parse(resp)
+            return sig.parse(resp)
 
-    return inner
+        return inner
+
+    return _ai_fn
