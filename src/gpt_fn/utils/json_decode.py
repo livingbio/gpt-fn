08ee66c1d766bdabe3ad7a2c4e4454d7ade5034d
lucemia <lucemia@gmail.com>
2023-10-19 10:11:18 +0800

    fix #72 use pushdown auto

--- src/gpt_fn/utils/json_decode.py
+++ src/gpt_fn/utils/json_decode.py
@@ -1,131 +1,210 @@
-"""This module contains functions to fix JSON strings using general programmatic approaches, suitable for addressing
-common JSON formatting issues."""
-from __future__ import annotations
 
-import contextlib
-import json
-import logging as logger
-import re
+from functools import wraps
 
+def state(fn):
+    @wraps(fn)
+    def wrapper(input: str, stack: list[str] = []) -> str:
+        r = fn(input, stack)
+        if r is None:
+            raise ValueError("Invalid JSON {input} in {fn.__name__}")
+        return r
+    return wrapper
 
-def extract_char_position(error_message: str) -> int:
-    """Extract the character position from the JSONDecodeError message.
+def state_start(input: str) -> str:
+    return state_root_object(input, ["$"])
 
-    Args:
-        error_message (str): The error message from the JSONDecodeError
-          exception.
+@state
+def state_root_object(input: str, stack: list[str]) -> str:
+    input = input.strip()
 
-    Returns:
-        int: The character position.
-    """
+    if input[0] == "{":
+        return input[0] + state_object(input[1:], stack + ["{"])
+    
+@state
+def state_finish(input: str, stack: list[str]) -> str:
+    input = input.strip()
 
-    char_pattern = re.compile(r"\(char (\d+)\)")
-    if match := char_pattern.search(error_message):
-        return int(match[1])
-    else:
-        raise ValueError("Character position not found in the error message.")
+    if not input:
+        return ""
 
+@state
+def state_object(input: str, stack: list[str]) -> str:
+    input = input.strip()
 
-def fix_invalid_escape(json_to_load: str, error_message: str) -> str:
-    """Fix invalid escape sequences in JSON strings.
+    if input[0] == "}" and stack[-1] == "{":
+        return input[0] + state_post_object(input[1:], stack[:-1])
 
-    Args:
-        json_to_load (str): The JSON string.
-        error_message (str): The error message from the JSONDecodeError
-          exception.
+    if input[0] == '"':
+        return input[0] + state_property_string(input[1:], stack)
 
-    Returns:
-        str: The JSON string with invalid escape sequences fixed.
-    """
-    while error_message.startswith("Invalid \\escape"):
-        bad_escape_location = extract_char_position(error_message)
-        json_to_load = json_to_load[:bad_escape_location] + json_to_load[bad_escape_location + 1 :]
-        try:
-            json.loads(json_to_load)
-            return json_to_load
-        except json.JSONDecodeError as e:
-            logger.debug("json loads error - fix invalid escape", e)
-            error_message = str(e)
-    return json_to_load
+@state
+def state_post_object(input: str, stack: list[str]) -> str:
+    if stack[-1] in {"{", "["}:
+        return state_post_value(input, stack)
 
+    if stack[-1] == "$":
+        return state_finish(input, stack)
+    
+@state
+def state_post_property(input: str, stack: list[str]) -> str:
+    input = input.strip()
 
-def balance_braces(json_string: str) -> str:
-    """
-    Balance the braces in a JSON string.
+    if input[0] == ":":
+        return input[0] + state_value(input[1:], stack)
 
-    Args:
-        json_string (str): The JSON string.
+@state
+def state_value(input: str, stack: list[str]) -> str:
+    input = input.strip()
 
-    Returns:
-        str: The JSON string with braces balanced.
-    """
+    if input[0] == "f":
+        assert input[:5] == "false"
+        return input[:5] + state_post_value(input[5:], stack)
+    if input[0] == "t":
+        assert input[:4] == "true"
+        return input[:4] + state_post_value(input[4:], stack)
+    if input[0] == "n":
+        assert input[:4] == "null"
+        return input[:4] + state_post_value(input[4:], stack)
+    if input[0].isdigit() or input[0] == "-":
+        return input[0] + state_int(input, stack)
+    if input[0] == '"':
+        return input[0] + state_value_string(input[1:], stack)
+    if input[0] == "{":
+        return input[0] + state_object(input[1:], stack + ["{"])
+    if input[0] == "[":
+        return input[0] + state_value(input[1:], stack + ["["]) 
+    if input[0] == "]":
+        assert stack[-1] == "["
+        return input[0] + state_post_value(input[1:], stack[:-1])
+    
+@state
+def state_post_value(input: str, stack: list[str]) -> str:
+    input = input.strip()
 
-    open_braces_count = json_string.count("{")
-    close_braces_count = json_string.count("}")
+    if input[0] == ",":
+        if stack[-1] == "[":
+            return input[0] + state_value(input[1:], stack)
+        if stack[-1] == "{":
+            return input[0] + state_object(input[1:], stack)
+        return
+    elif input[0] == "]":
+        assert stack[-1] == "["
+        return input[0] + state_post_value(input[1:], stack[:-1])
+    elif input[0] == "}":
+        assert stack[-1] == "{"
+        return input[0] + state_post_object(input[1:], stack[:-1])
 
-    while open_braces_count > close_braces_count:
-        json_string += "}"
-        close_braces_count += 1
+@state
+def state_value_string(input: str, stack: list[str]) -> str:
+    if input[0] == '"':
+        # TODO: 
+        return input[0] + state_post_value(input[1:], stack)
 
-    while close_braces_count > open_braces_count:
-        json_string = json_string.rstrip("}")
-        close_braces_count -= 1
+    if input[0] == "\\":
+        return input[0] + state_value_string(input[1:], stack + ["v"])
+    
+    return input[0] + state_value_string(input[1:], stack)
+    
+def state_property_string(input: str, stack: list[str]) -> str:
+    if input[0] == '"':
+        return input[0] + state_post_property(input[1:], stack)
 
-    with contextlib.suppress(json.JSONDecodeError):
-        json.loads(json_string)
-        return json_string
+    if input[0] == "\\":
+        return input[0] + state_property_string(input[1:], stack + ["p"])
+    
+    return input[0] + state_property_string(input[1:], stack)
 
-    return json_string
+@state
+def state_escape_char(input: str, stack: list[str]) -> str:
+    if input[0] == "u":
+        int(input[1:5], 16)
+    
+        if stack[-1] == "v":
+            return input[0:5] + state_value_string(input[5:], stack[:-1]) 
+        if stack[-1] == "p":
+            return input[0] + state_property_string(input[5:], stack[:-1])
 
+        return        
+    if input[0] in {"\\", "/", '"', "b", "f", "n", "r", "t"}:
+        if stack[-1] == "v":
+            return input[0] + state_value_string(input[1:], stack[:-1])
+        if stack[-1] == "p":
+            return input[0] + state_property_string(input[1:], stack[:-1])
+        return
 
-def add_quotes_to_property_names(json_string: str) -> str:
-    """
-    Add quotes to property names in a JSON string.
+@state
+def state_int(input: str, stack: list[str]) -> str:
+    if input[0].isdigit():
+        return input[0] + state_int(input[1:], stack)
+    if input[0] == ".":
+        return input[0] + state_double(input[1:], stack)
+    if input[0] == ",":
+        if stack[-1] == "[":
+            return input[0] + state_value(input[1:], stack)
+        if stack[-1] == "{":
+            return input[0] + state_object(input[1:], stack)
+        return
+    if input[0] == "}":
+        assert stack[-1] == "{"
+        return input[0] + state_post_int_parent(input[1:], stack)
+    if input[0] == "]":
+        assert stack[-1] == "["
+        return input[0] + state_post_int_parent(input[1:], stack)
+    if input[0].isspace():
+        return input[0] + state_post_value(input[1:], stack)
+    
+@state
+def state_post_int_parent(input: str, stack: list[str]) -> str:
+    if stack[-1] == "[":
+        return state_post_value(input, stack[:-1])
+    if stack[-1] == "{":
+        return state_post_object(input, stack[:-1])
+    
+@state
+def state_double(input: str, stack: list[str]) -> str:
+    if input[0].isdigit():
+        return input[0] + state_double(input[1:], stack)
+    if input[0] == ",":
+        if stack[-1] == "[":
+            return input[0] + state_value(input[1:], stack)
+        if stack[-1] == "{":
+            return input[0] + state_object(input[1:], stack)
+        return
+    if input[0] == "}":
+        assert stack[-1] == "{"
+        return input[0] + state_post_int_parent(input[1:], stack)
+    if input[0] == "]":
+        assert stack[-1] == "["
+        return input[0] + state_post_int_parent(input[1:], stack)
+    if input[0].isspace():
+        return input[0] + state_post_value(input[1:], stack)
+    if input[0] == {"e", "E"}:
+        return input[0] + state_exponent_sign(input[1:], stack)
+    
+@state
+def state_exponent_sign(input: str, stack: list[str]) -> str:
+    if input[0] in {"+", "-"}:
+        return input[0] + state_exponent_digits(input[1:], stack)
 
-    Args:
-        json_string (str): The JSON string.
+@state
+def state_exponent_digits(input: str, stack: list[str]) -> str:
+    if input[0].isdigit():
+        return input[0] + state_exponent_digits(input[1:], stack)
+    if input[0] == ",":
+        if stack[-1] == "[":
+            return input[0] + state_value(input[1:], stack)
+        if stack[-1] == "{":
+            return input[0] + state_object(input[1:], stack)
+        return
+    if input[0] == "}":
+        assert stack[-1] == "{"
+        return input[0] + state_post_int_parent(input[1:], stack)
+    if input[0] == "]":
+        assert stack[-1] == "["
+        return input[0] + state_post_int_parent(input[1:], stack)
+    if input[0].isspace():
+        return input[0] + state_post_value(input[1:], stack)
+    
 
-    Returns:
-        str: The JSON string with quotes added to property names.
-    """
-
-    def replace_func(match: re.Match[str]) -> str:
-        return f'"{match[1]}":'
-
-    property_name_pattern = re.compile(r"(\w+):")
-    corrected_json_string = property_name_pattern.sub(replace_func, json_string)
-
-    try:
-        json.loads(corrected_json_string)
-        return corrected_json_string
-    except json.JSONDecodeError as e:
-        raise e
-
-
-def correct_json(json_to_load: str) -> str:
-    """
-    Correct common JSON errors.
-    Args:
-        json_to_load (str): The JSON string.
-    """
-
-    try:
-        logger.debug("json", json_to_load)
-        json.loads(json_to_load)
-        return json_to_load
-    except json.JSONDecodeError as e:
-        logger.debug("json loads error", e)
-        error_message = str(e)
-        if error_message.startswith("Invalid \\escape"):
-            json_to_load = fix_invalid_escape(json_to_load, error_message)
-        if error_message.startswith("Expecting property name enclosed in double quotes"):
-            json_to_load = add_quotes_to_property_names(json_to_load)
-            try:
-                json.loads(json_to_load)
-                return json_to_load
-            except json.JSONDecodeError as e:
-                logger.debug("json loads error - add quotes", e)
-                error_message = str(e)
-        if balanced_str := balance_braces(json_to_load):
-            return balanced_str
-    return json_to_load
+def correct_json(json_str: str) -> str:
+    return state_start(json_str)
--- src/gpt_fn/utils/tests/test_json_decode.py
+++ src/gpt_fn/utils/tests/test_json_decode.py
@@ -1,13 +1,33 @@
 from pathlib import Path
-
+from typing import Any
 import pytest
 from syrupy.assertion import SnapshotAssertion
-
+import json
 from ..json_decode import correct_json
 
+def load_fixed_json(json_str: str) -> dict[str, Any]:
+    return json.loads(correct_json(json_str))
 
-@pytest.mark.parametrize("test_filename", (Path(__file__).parent / "test_json_decode").glob("*.txt"))
-def test_correct_json(snapshot: SnapshotAssertion, test_filename: Path) -> None:
+
+def test_fixed_json_simple_case(snapshot: SnapshotAssertion) -> None:
+    assert snapshot == load_fixed_json("{}")
+    assert snapshot == load_fixed_json('{"foo": "bar"}')
+    assert snapshot == load_fixed_json('{"foo": "bar", "baz": "qux"}')
+    assert snapshot == load_fixed_json('{"foo": "bar", "baz": "qux", "quux": "corge"}')
+    # add more nested objects
+    assert snapshot == load_fixed_json('{"foo": {"bar": "baz"}}')
+    assert snapshot == load_fixed_json('{"foo": {"bar": {"baz": "qux"}}}')
+    assert snapshot == load_fixed_json('{"foo": {"bar": {"baz": {"qux": "quux"}}}}')
+
+@pytest.mark.parametrize("test_filename", (Path(__file__).parent / "test_json_decode").glob("safe/*.json"), ids=lambda x: x.name)
+def test_fixed_json_safe_case(snapshot: SnapshotAssertion, test_filename: Path) -> None:
     content = test_filename.read_text()
-    result = correct_json(content)
+    result = load_fixed_json(content)
     assert snapshot == result
+
+
+@pytest.mark.parametrize("test_filename", (Path(__file__).parent / "test_json_decode").glob("unsafe/*.json"), ids=lambda x: x.name)
+def test_fixed_json_unsafe_case(snapshot: SnapshotAssertion, test_filename: Path) -> None:
+    content = test_filename.read_text()
+    result = load_fixed_json(content)
+    assert snapshot == json.loads(result)
